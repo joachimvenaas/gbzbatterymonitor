e164bc2b430ef21cae352b8e4f86eb57ffb4994e
joachimvenaas <joachimvenaas@gmail.com>
2016-12-06 13:50:10 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 """
 " Edit below this line to fit your needs
 """
-# Path to pngview (raspidmx)
+# Path to pngview (raspidmx) and icons
 PNGVIEWPATH = "/home/pi/raspidmx/pngview"
 ICONPATH = "/home/pi/gbzbattery/icons"
 
@@ -11,21 +11,14 @@ ICON = 1
 
 # GPIO (BOARD numbering scheme) pin for good voltage LED
 GOODVOLTPIN = 16
-
-# GPIO (BOARD numbering scheme) pin for low voltage LED
 LOWVOLTPIN = 15
-
-## Define voltages
-# Number of batteries in series
-BATNUMBER = 1
 
 # Fully charged voltage (for a single battery); 
 #   i.e. 1.4V for a NiMH battery, or 4.2V for a LiPo battery
 FULLBATVOLT = 4.1
 
 # Discharged voltage (for a single battery); 
-#   i.e. 1.05V for a NiMH battery, or 3.4V for a LiPo battery
-#   You should use a conservative value in order to avoid destructive
+#   i.e. 1.05V for a NiMH battery, or 3.4V for a LiPo battery#   You should use a conservative value in order to avoid destructive
 #   discharging
 LOWBATVOLT = 3.4
 
@@ -48,25 +41,17 @@ HIGHRESVAL = 5600
 
 # Voltage value measured by the MCP3008 when batteries are fully charged
 # It should be near 3.3V due to Raspberry Pi GPIO compatibility)
-VHIGHBAT = (BATNUMBER*FULLBATVOLT)*(HIGHRESVAL)/(LOWRESVAL+HIGHRESVAL)
+VHIGHBAT = (FULLBATVOLT)*(HIGHRESVAL)/(LOWRESVAL+HIGHRESVAL)
 
 # Voltage value measured by the MCP3008 when batteries are discharged
-VLOWBAT = (BATNUMBER*LOWBATVOLT)*(HIGHRESVAL)/(LOWRESVAL+HIGHRESVAL)
+VLOWBAT = (LOWBATVOLT)*(HIGHRESVAL)/(LOWRESVAL+HIGHRESVAL)
 
 # Voltage value measured by the MCP3008 when batteries voltage is dangerously
 #   low
-VDNGBAT = (BATNUMBER*DNGBATVOLT)*(HIGHRESVAL)/(LOWRESVAL+HIGHRESVAL)
+VDNGBAT = (DNGBATVOLT)*(HIGHRESVAL)/(LOWRESVAL+HIGHRESVAL)
 
 # ADC voltage reference (3.3V for Raspberry Pi)
 ADCVREF = 3.3
-
-# Compensation due to the difference between ADC voltage reference and 
-#   max value for voltage through resistor divider
-#   i.e. : with the given resistances values, VREF is 3.3 V, and max voltage
-#   through resistor divider is about 3.14V, leading to about 4.8% deviation.
-# This compensation is used to correct computed battery voltage return by
-#   network queries
-VCOMP = 1+(ADCVREF-VHIGHBAT)/ADCVREF
 
 ## Define expected ADC values
 # MCP23008 channel to use (from 0 to 7)
@@ -76,19 +61,11 @@ ADCCHANNEL = 0
 #  * 3.3 is the reference voltage (got from Raspberry Pi's +3.3V power line)
 #  * 1024.0 is the number of possible values (MCP23008 is a 10 bit ADC)
 ADCHIGH = VHIGHBAT / (ADCVREF / 1024.0)
-
-# MCP23008 should return this value when batteries are discharged
-#  * 3.3 is the reference voltage (got from Raspberry Pi's +3.3V power line)
-#  * 1024.0 is the number of possible values (MCP23008 is a 10 bit ADC)
 ADCLOW = VLOWBAT / (ADCVREF / 1024.0)
-
-# MCP23008 should return this value when batteries atteigns dangerous voltage
-#  * 3.3 is the reference voltage (got from Raspberry Pi's +3.3V power line)
-#  * 1024.0 is the number of possible values (MCP23008 is a 10 bit ADC)
 ADCDNG = VDNGBAT / (ADCVREF / 1024.0)
 
-# Refresh rate (ms)
-REFRESH_RATE = 2000
+# Refresh rate (s)
+REFRESH_RATE = 2
 
 # Display some debug values when set to 1, and nothing when set to 0
 DEBUGMSG = 0
