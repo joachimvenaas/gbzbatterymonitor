d9fba425359163d9e8cceea23ebcf2f1fba7127a
joachimvenaas <joachimvenaas@gmail.com>
2016-12-06 17:13:59 +0100

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -9,16 +9,20 @@ import math
 from config import *
 from mcp3008 import *
 
-ret = 0
 
-print("Batteries high voltage:       " + str(VHIGHBAT))
-print("Batteries low voltage:        " + str(VLOWBAT))
-print("Batteries dangerous voltage:  " + str(VDNGBAT))
-print("ADC high voltage value:       " + str(ADCHIGH))
-print("ADC low voltage value:        " + str(ADCLOW))
-print("ADC dangerous voltage value:  " + str(ADCDNG))
+
+print("Batteries 100% voltage:      " + str(VOLT100))
+print("Batteries 75% voltage:       " + str(VOLT75))
+print("Batteries 50% voltage:       " + str(VOLT50))
+print("Batteries 25% voltage:       " + str(VOLT25))
+print("Batteries dangerous voltage: " + str(VOLT0))
+print("ADC 100% value:              " + str(ADC100))
+print("ADC 75% value:               " + str(ADC75))
+print("ADC 50% value:               " + str(ADC50))
+print("ADC 25% value:               " + str(ADC25))
+print("ADC dangerous voltage value: " + str(ADC0))
 print(" ")
-print("Time	ADC	Volt")
+print("Time	    ADC     Volt")
 while True:
     ret1 = readadc(ADCCHANNEL, SPICLK, SPIMOSI, SPIMISO, SPICS)
     time.sleep(3)
@@ -27,6 +31,6 @@ while True:
     ret3 = readadc(ADCCHANNEL, SPICLK, SPIMOSI, SPIMISO, SPICS)
     ret = ret1 + ret2 + ret3
     ret = ret/3
-    print(strftime("%H:%M", localtime()) + "	" + str(ret) + "	" + str((ADCVREF / 1024.0) * ret) + "V")
+    print("Time     " + str(ret) + "    " + str(((HIGHRESVAL+LOWRESVAL)*ret*(ADCVREF/1024))/HIGHRESVAL))
         
     time.sleep(1)
